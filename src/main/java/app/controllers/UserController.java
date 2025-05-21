f510b105bf2775aea775c154a4e875a254548054
Zuhami <Aymanchaykh6@gmail.com>
2025-05-21 10:41:00 +0200

    sider

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -16,7 +16,7 @@ public class UserController {
 
         app.post("login", ctx -> login(ctx, connectionPool));
         app.get("logout", ctx -> logout(ctx));
-        app.get(login, ctx ->ctx.render("login.html"));
+        app.get("login", ctx ->ctx.render("login.html"));
         app.get("createuser", ctx -> ctx.render("createuser.html"));
         app.post("createuser", ctx -> createUser(ctx, connectionPool));
 
