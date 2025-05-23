bf350f01f270f1438ca90d57eda56319cf81cc83
KhuongNDang <khndang@gmail.com>
2025-05-23 10:26:35 +0200

    Merging codes together

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -39,8 +39,6 @@ public class UserController {
         app.get("/login", ctx -> ctx.render("login.html"));
         app.get("createuser", ctx -> ctx.render("createuser.html"));
         app.get("/orderSite", ctx -> ctx.render("orderSite.html"));
-        app.get("/orderSite2", ctx -> ctx.render("orderSite2.html"));
-        app.get("/orderSite3", ctx -> ctx.render("orderSite3.html"));
         app.get("/orderConfirmation", ctx -> ctx.render("orderConfirmation.html"));
         app.get("/customerprofile", ctx -> CustomerProfileController.showProfile(ctx, connectionPool));
         app.post("/createuser", ctx -> UserController.createUser(ctx, connectionPool));
